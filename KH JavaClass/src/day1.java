6514b42229e4a0706fdfae24f197d66ba88e9c72
82102 <82102@10.16.15.128>
2021-04-02 21:14:26 +0900

    2021/04/02

---
+++ KH JavaClass/src/day1.java
@@ -0,0 +1,13 @@
+public class day1 {
+
+	public static void main(String[] args) {
+		int num1 = 12;
+		int num2 = 10;
+		int result = num1 + num2; //메모리에 저장된 num1 , num2 값으로 계산하여 새로운 메모리에 저장
+		
+		System.out.println(num1 + " + " + num2 + " = " + result); //숫자열 출력
+		System.out.printf("%d + %d = %d", num1, num2, result);   //문자열 출력
+		
+	}
+
+}
